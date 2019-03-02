1d7f634edc2300f9b6bff8a17368ccc14764312a
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-03 09:29:03 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -16,14 +16,16 @@ function gx(x) {
   return window.innerWidth/2+x;
 }
 function gy(y) {
-  return window.innerHeigh/2-y;
+  return window.innerHeight/2-y;
 }
 function startscreen() {
+  ctx.fillStyle = "#e0f1ff";
+  ctx.rect(0,0,window.innerWidth,window.innerHeight)
   try {
     var img = document.getElementById("logo");
-    ctx.drawImage(img, gx(0-204), 40,408,120);
+    ctx.drawImage(img, gx(0-204), 80,408,120);
     var img = document.getElementById("pressz");
-    ctx.drawImage(img, gx(0-50), 340,100,50);
+    ctx.drawImage(img, gx(0-50), 390,100,50);
   } catch {}
 }
 
