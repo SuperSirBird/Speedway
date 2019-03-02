03ac2c5dc9dccb2edab84638b5d1b68ea0f31f9a
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-03 00:57:33 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -1,3 +1,16 @@
+/*
+
+Engine by SuperSirBird
+
+*/
+
+// Setup Canvas
+
+var c = document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+c.width  = window.innerWidth;
+c.height = window.innerHeight;
+
 function step() {
   window.requestAnimationFrame(step);
 }
