651dd190f5f50145e059e7850a768085c0fc8849
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-03 08:18:33 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -4,14 +4,13 @@ Engine by SuperSirBird
 
 */
 
-alert("running");
-
 // Setup Canvas
 
 var c = document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 c.width  = window.innerWidth;
 c.height = window.innerHeight;
+ctx.imageSmoothingEnabled = false;
 
 function step() {
   ctx.clearRect(0, 0, c.width, c.height);
