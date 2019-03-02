279365af13e9dec30d87b0fba9d3200cfb632862
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-03 08:58:04 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -12,13 +12,20 @@ c.width  = window.innerWidth;
 c.height = window.innerHeight;
 ctx.imageSmoothingEnabled = false;
 
+function gx(x) {
+  return window.innerWidth/2+x;
+}
+function gy(y) {
+  return window.innerHeigh/2-y;
+}
+
 function step() {
   ctx.clearRect(0, 0, c.width, c.height);
   
   // Draw logo
   try {
     var img = document.getElementById("logo");
-    ctx.drawImage(img, 10, 10,408,120);
+    ctx.drawImage(img, gx(0-204), 40,408,120);
   } catch {}
   
   // Render
