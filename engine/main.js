75a3c6124ebd6167e42c06ab180a83564e4f9dab
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-03 09:07:11 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -18,17 +18,20 @@ function gx(x) {
 function gy(y) {
   return window.innerHeigh/2-y;
 }
+funtion startscreen() {
+  try {
+    var img = document.getElementById("logo");
+    ctx.drawImage(img, gx(0-204), 40,408,120);
+    var img = document.getElementById("pressz");
+    ctx.drawImage(img, gx(0-50), 340,100,50);
+  } catch {}
+}
 
 function step() {
   ctx.clearRect(0, 0, c.width, c.height);
   
-  // Draw logo
-  try {
-    var img = document.getElementById("logo");
-    ctx.drawImage(img, gx(0-204), 40,408,120);
-  } catch {}
-  
   // Render
+  startscreen();
   
   
   window.requestAnimationFrame(step);
