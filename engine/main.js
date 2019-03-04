5e38628ee13ea23c97b5ff1710307a1987da3c07
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-04 14:22:47 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -43,7 +43,7 @@ function playg() {
       perspec(roadx[i-1],roady[i-1]-200,roadz[i-1]);
       var x2 = x;
       var y2 = y;
-      ctx.fillStyle = 'rgb(255, 165, ' + roadz[i] + ')';
+      ctx.fillStyle = 'rgba(255, 165, ' + roadz[i] + ',50)';
       ctx.fillRect(0,gy(y1),window.innerWidth,gy(y1)-gy(y2));
       
     }
