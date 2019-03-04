a9e8e858c6133785651a428856db1463c368e411
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-04 13:55:55 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -36,10 +36,10 @@ function playg() {
   
   for (var i = roadx.length-1;i>1;i-=1) {
     if (roadz[i-1]-mz > 0) {
-      perspec(roadx[i],roady[i],roadz[i]);
+      perspec(roadx[i],roady[i]-60,roadz[i]);
       var x1 = x;
       var y1 = y;
-      perspec(roadx[i-1],roady[i-1],roadz[i-1]);
+      perspec(roadx[i-1],roady[i-1]-60,roadz[i-1]);
       var x2 = x;
       var y2 = y;
       
