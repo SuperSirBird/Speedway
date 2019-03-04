fa6fed551ed3a0f81dc3bc65d480b81e34bf9110
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-04 14:09:13 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -43,9 +43,10 @@ function playg() {
       perspec(roadx[i-1],roady[i-1]-60,roadz[i-1]);
       var x2 = x;
       var y2 = y;
-      
-      ctx.fillStyle = "#effdff";
-      ctx.fillRect(0,gy(y1),window.innerWidth,gy(y1)-gy(y2));
+      if (roadx[i] % 20 === 0) {
+        ctx.fillStyle = "#4ebc00";
+        ctx.fillRect(0,gy(y1),window.innerWidth,gy(y1)-gy(y2));
+      }
       
       
     }
