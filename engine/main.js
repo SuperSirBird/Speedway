8cbc83f8e55ef8d314eb28352399e76ff8e0ead1
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-03-06 08:57:04 +1100

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -45,10 +45,10 @@ var xspeed = 0;
 function controls() {
   xspeed = xspeed/1.1;
   if (keys[39]) { //right
-    xspeed += 3;
+    xspeed += 3.4;
   } 
   if (keys[37]) { //left
-    xspeed += -3;
+    xspeed += -3.4;
   } 
   mx += xspeed
 }
